c1c5c46d0380186fd65854292121fc8e62ada452
veyndan <veyndan89@gmail.com>
2016-05-10 18:51:55 +0100

    Refactor file names for consistency

--- app/src/main/java/com/veyndan/hermes/home/HomeActivity.java
+++ app/src/main/java/com/veyndan/hermes/home/HomeActivity.java
@@ -19,7 +19,7 @@ public class HomeActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.home_activity);
         ButterKnife.bind(this);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
