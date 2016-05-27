7e8ac4faffe430857eedc2d97590df6eade690da
veyndan <veyndan89@gmail.com>
2016-05-27 16:18:27 +0100

    Seperate api service logic from Activity

--- app/src/main/java/com/veyndan/hermes/home/HomeActivity.java
+++ app/src/main/java/com/veyndan/hermes/home/HomeActivity.java
@@ -6,23 +6,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.ryanharter.auto.value.moshi.AutoValueMoshiAdapterFactory;
-import com.squareup.moshi.Moshi;
 import com.veyndan.hermes.BaseActivity;
 import com.veyndan.hermes.Comic;
 import com.veyndan.hermes.R;
+import com.veyndan.hermes.service.ComicService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.moshi.MoshiConverterFactory;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
-import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -33,22 +26,6 @@ public class HomeActivity extends BaseActivity {
     private final List<Comic> comics = new ArrayList<>();
     private final HomeAdapter adapter = new HomeAdapter(comics);
 
-    private final Moshi moshi = new Moshi.Builder().add(new AutoValueMoshiAdapterFactory()).build();
-
-    private final Retrofit retrofit = new Retrofit.Builder()
-            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .baseUrl("https://xkcd.com/")
-            .build();
-
-    private interface XKCDService {
-        @GET("info.0.json")
-        Observable<Comic> latest();
-
-        @GET("{num}/info.0.json")
-        Observable<Comic> num(@Path("num") int num);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,17 +35,10 @@ public class HomeActivity extends BaseActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
-        final XKCDService xkcdService = retrofit.create(XKCDService.class);
+        ComicService comicService = new ComicService();
 
-        xkcdService.latest()
+        comicService.fetchComics(1600)
                 .compose(this.<Comic>bindToLifecycle())
-                .flatMap(comic -> {
-                    Observable<Comic> observable = Observable.empty();
-                    for (int num = comic.num(); num > comic.num() - 100; num--) {
-                        observable = observable.concatWith(xkcdService.num(num));
-                    }
-                    return observable;
-                })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(comic -> {
---
+++ app/src/main/java/com/veyndan/hermes/service/ComicService.java
@@ -0,0 +1,82 @@
+package com.veyndan.hermes.service;
+
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+
+import com.ryanharter.auto.value.moshi.AutoValueMoshiAdapterFactory;
+import com.squareup.moshi.Moshi;
+import com.veyndan.hermes.Comic;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+public class ComicService {
+
+    private static final String XKCD_BASE_URL = "https://xkcd.com";
+
+    private final XKCDService xkcdService;
+
+    private interface XKCDService {
+        @GET("info.0.json")
+        Observable<Comic> latest();
+
+        @GET("{num}/info.0.json")
+        Observable<Comic> num(@Path("num") int num);
+    }
+
+    public ComicService() {
+
+        final Moshi moshi = new Moshi.Builder().add(new AutoValueMoshiAdapterFactory()).build();
+
+        final Retrofit retrofit = new Retrofit.Builder()
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .baseUrl(XKCD_BASE_URL)
+                .build();
+
+        xkcdService = retrofit.create(XKCDService.class);
+    }
+
+    /**
+     * Fetches every xkcd comic ever made.
+     *
+     * @return An Observable which emits each comic individually.
+     */
+    @NonNull
+    public Observable<Comic> fetchComics() {
+        return fetchComics(1);
+    }
+
+    /**
+     * Fetches every xkcd comic from the desired comic up to and including the most recent comic.
+     *
+     * @param from The earliest comic to fetch from (inclusive).
+     * @return An Observable which emits each comic individually.
+     */
+    @NonNull
+    public Observable<Comic> fetchComics(@IntRange(from = 1) int from) {
+        return xkcdService.latest()
+                .flatMap(comic -> fetchComics(from, comic.num()));
+    }
+
+    /**
+     * Fetches every xkcd comic from the desired comic up to the desired comic.
+     *
+     * @param from The earliest comic to fetch from (inclusive).
+     * @param to   The latest comic to fetch to (inclusive).
+     * @return An Observable which emits each comic individually.
+     */
+    @NonNull
+    public Observable<Comic> fetchComics(@IntRange(from = 1) int from, @IntRange(from = 1) int to) {
+        Observable<Comic> observable = Observable.empty();
+        for (int num = to; num >= from; num--) {
+            observable = observable.concatWith(xkcdService.num(num));
+        }
+        return observable;
+    }
+
+}
