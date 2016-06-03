08c082d35f287dd4cf94bee85448777b6f82679c
veyndan <veyndan89@gmail.com>
2016-06-03 19:47:23 +0100

    Add override annotations to subclassed methods

--- app/src/main/java/com/veyndan/hermes/database/DbHelper.java
+++ app/src/main/java/com/veyndan/hermes/database/DbHelper.java
@@ -24,15 +24,18 @@ public final class DbHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE);
     }
 
+    @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + Comic.TABLE);
         onCreate(db);
     }
 
+    @Override
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         onUpgrade(db, oldVersion, newVersion);
     }
